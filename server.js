8a8e49f760c613568d5ca97fa907bd635d6b5359
alexandr <programalexandr@gmail.com>
2023-11-28 14:44:48 -0500

    url updated and procfile added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 //connecting to mongo db
-mongoose.connect('mongodb://127.0.0.1:27017/newapp').then(()=> console.log('mongoDB connected'))
+mongoose.connect('mongodb+srv://Apet:admin@cluster0.jkgjiv5.mongodb.net').then(()=> console.log('mongoDB connected'))
     .catch(err => console.log(err));
 
 const userSchema = new mongoose.Schema({
